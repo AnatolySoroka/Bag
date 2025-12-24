d03e09bb3ef7566913278c331448d82bb7f7fc45
Anatolii Soroka <toliksoroka66@gmail.com>
2025-12-24 22:19:33 +0200

    adaptive mobile menu

--- js/main.js
+++ js/main.js
@@ -1,31 +1,40 @@
-const mixer = mixitup('.categories', {
-  load: {
-    filter: '.water-resistant'
-  }
+// const mixer = mixitup('.categories', {
+//   load: {
+//     filter: '.water-resistant'
+//   }
+// });
+
+// const carts = document.querySelectorAll('.our__item');
+
+// function generateRGB(min, max) {
+//   return Math.floor(Math.random() * (max - min) + min);
+// }
+
+// function getRandomColor() {
+//   return rgbToHex(
+//     generateRGB(127, 255),
+//     generateRGB(127, 255),
+//     generateRGB(127, 255)
+//   );
+// }
+
+// function rgbToHex(r, g, b) {
+//   return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+// }
+
+// function toHex(value) {
+//   return value.toString(16).padStart(2, "0");
+// }
+
+// for (let i = 0; i < carts.length; i++) {
+//   carts[i].style.backgroundColor = getRandomColor();
+// }
+
+
+
+
+const burger = document.querySelector('.burger');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('is-open');
 });
-
-const carts = document.querySelectorAll('.our__item');
-
-function generateRGB(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
-function getRandomColor() {
-  return rgbToHex(
-    generateRGB(127, 255),
-    generateRGB(127, 255),
-    generateRGB(127, 255)
-  );
-}
-
-function rgbToHex(r, g, b) {
-  return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
-}
-
-function toHex(value) {
-  return value.toString(16).padStart(2, "0");
-}
-
-for(let i = 0; i < carts.length; i++) {
-    carts[i].style.backgroundColor = getRandomColor();
-}
