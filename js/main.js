2525542360ca1718286d814cc99b3ed29c729290
Anatolii Soroka <toliksoroka66@gmail.com>
2025-12-25 19:33:34 +0200

    scroll lock when opening the burger menu

--- js/main.js
+++ js/main.js
@@ -1,40 +1,45 @@
-// const mixer = mixitup('.categories', {
-//   load: {
-//     filter: '.water-resistant'
-//   }
-// });
+const mixer = mixitup('.categories', {
+  load: {
+    filter: '.water-resistant'
+  }
+});
 
-// const carts = document.querySelectorAll('.our__item');
+const carts = document.querySelectorAll('.our__item');
 
-// function generateRGB(min, max) {
-//   return Math.floor(Math.random() * (max - min) + min);
-// }
+function generateRGB(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
-// function getRandomColor() {
-//   return rgbToHex(
-//     generateRGB(127, 255),
-//     generateRGB(127, 255),
-//     generateRGB(127, 255)
-//   );
-// }
+function getRandomColor() {
+  return rgbToHex(
+    generateRGB(127, 255),
+    generateRGB(127, 255),
+    generateRGB(127, 255)
+  );
+}
 
-// function rgbToHex(r, g, b) {
-//   return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
-// }
+function rgbToHex(r, g, b) {
+  return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+}
 
-// function toHex(value) {
-//   return value.toString(16).padStart(2, "0");
-// }
+function toHex(value) {
+  return value.toString(16).padStart(2, "0");
+}
 
-// for (let i = 0; i < carts.length; i++) {
-//   carts[i].style.backgroundColor = getRandomColor();
-// }
-
-
+for (let i = 0; i < carts.length; i++) {
+  carts[i].style.backgroundColor = getRandomColor();
+}
 
 
+
+const body = document.querySelector('body');
 const burger = document.querySelector('.burger');
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('is-open');
+  if(burger.classList.contains('is-open')) {
+    body.classList.add('active');
+  } else {
+    body.classList.remove('active');
+  }
 });
